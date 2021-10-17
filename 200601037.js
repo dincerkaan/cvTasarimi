a19433720d2c9a18c743eec381a4515f0f7ac88b
Dinçer Kaan Turanlı <80959458+dincerkaan@users.noreply.github.com>
2021-10-17 16:35:02 +0300

    Add files via upload
    Web Tabanlı Programlamaya Giriş Ödevi - CV Tasarım Örneği

---
+++ 200601037.js
@@ -0,0 +1,10 @@
+function banaUlasin(){
+
+    let adSoyad = document.getElementById("adSoyadGiris").value;
+    let email = document.getElementById("emailGiris").value;
+    let telefon = document.getElementById("telefonGiris").value;
+    let mesaj = document.getElementById("mesajGiris").value;
+
+    alert("Adınız ve Soyadınız: " + adSoyad + "\n" + "Email Adresiniz: " + email + "\n" + 
+    "Telefon Numaranız: " + telefon + "\n" + "Mesajınız: " + mesaj);
+}
